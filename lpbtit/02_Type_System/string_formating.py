0693b24d6310a1feadb5d6f2f3c8035ef55fcdfb
erikboren <erik.boren@gmail.com>
2022-11-16 19:24:18 +0100

    match and fstrings

---
+++ lpbtit/02_Type_System/string_formating.py
@@ -0,0 +1,18 @@
+name = "Louis"
+age = 13
+dog: str = "Chiko"
+
+
+print(f"Hello {name}")
+
+print(f"{name} is {age} years old")
+
+print(f"{name} has a dog named {dog}")
+
+print(
+    f"""
+Hello {name}
+{name} is {age} years old
+{name} has a dog named {dog}
+"""
+)
---
+++ lpbtit/02_Type_System/variables.py
@@ -0,0 +1,2 @@
+box = 'ballons'
+print(box)
